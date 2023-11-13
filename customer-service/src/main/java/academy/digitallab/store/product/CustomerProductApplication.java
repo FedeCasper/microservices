8f3335ff9a49aa305097b0cc328389d9ed9673d6
FedeCasper <federicorouyere@gmail.com>
2023-11-12 21:50:40 -0300

    Customer-service

---
+++ customer-service/src/main/java/academy/digitallab/store/product/CustomerProductApplication.java
@@ -0,0 +1,17 @@
+package academy.digitallab.store.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProductApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CustomerProductApplication.class, args);
+
+		System.out.println();
+		System.out.println("// --> HELO WORLD!!! <-- //");
+	}
+
+}
